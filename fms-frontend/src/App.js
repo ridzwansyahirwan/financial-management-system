84c5314d22d4cf7bfcca3ed2f959153f590579a9
ridzwansyahirwan <73976489+ridzwansyahirwan@users.noreply.github.com>
2023-11-17 17:44:38 +0800

    Update App.js

--- fms-frontend/src/App.js
+++ fms-frontend/src/App.js
@@ -1,36 +1,29 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
-import Dashboard from './components/Dashboard'
-import Landing from './components/LandingPage'
-import Budgeting from './components/Budgeting'
-import Expense from './components/Expense'
-import Report from './components/Report'
-import AddExpense from './components/AddExpense.js'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import Landing from './components/LandingPage';
+import Budgeting from './components/Budgeting';
+import Expense from './components/Expense';
+import Report from './components/Report';
+import AddExpense from './components/AddExpense.js';
 
-
-function App() {  
-  return (    
+function App() {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing />}>
-          </Route>
-          <Route path='/Login' element={<Login />}>
-          </Route>            
-        <Route path='/Register' element={<Register />}>
-          </Route>
-        <Route path='/Dashboard' element={<Dashboard />}>
-          </Route>
-        <Route path='/Budgeting' element={<Budgeting />}>
-          </Route>
-        <Route path='/Expense' element={<Expense />}>
-          </Route>
-        <Route path='/Report' element={<Report />}>
-          </Route>
-        <Route path='/AddExpense' element={<AddExpense />}>
-          </Route>
+        <Route path="/" element={<Landing />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Budgeting" element={<Budgeting />} />
+        <Route path="/Expense" element={<Expense />} />
+        <Route path="/Report" element={<Report />} />
+        <Route path="/AddExpense" element={<AddExpense />} />
       </Routes>
-    </BrowserRouter>  )}
+    </BrowserRouter>
+  );
+}
 
-export default App
+export default App;
