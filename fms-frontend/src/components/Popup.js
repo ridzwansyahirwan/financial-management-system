ea75828dfa8d5bf84f1f12faadae6f21d4902922
ridzwansyahirwan <73976489+ridzwansyahirwan@users.noreply.github.com>
2023-11-20 07:59:37 +0800

    Code cleaning
    clean the code

--- fms-frontend/src/components/Popup.js
+++ fms-frontend/src/components/Popup.js
@@ -1,5 +1,4 @@
 const Popup = ({ isOpen, closeModal }) => {
-    // Modal JSX code here
     return (
         <div className="modal" style={{ display: isOpen ? 'block' : 'none' }}>
             <div className="modal-content">
@@ -7,7 +6,6 @@ const Popup = ({ isOpen, closeModal }) => {
                     &times;
                 </span>
                 <p>This is a popup/modal!</p>
-                {/* Add your content here */}
             </div>
         </div>
     );
--- fms-frontend/src/components/RegistrationValidation.js
+++ fms-frontend/src/components/RegistrationValidation.js
@@ -2,16 +2,13 @@ function Validation(values) {
     let errors = {};
   
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
   
-    // Validate name
     if (!values.name) {
       errors.name = "Name should not be empty";
     } else {
       errors.name = "";
     }
   
-    // Validate email
     if (!values.email) {
       errors.email = "Email should not be empty";
     } else if (!emailPattern.test(values.email)) {
@@ -20,13 +17,9 @@ function Validation(values) {
       errors.email = "";
     }
   
-    // Validate password
     if (!values.password) {
       errors.password = "Password should not be empty";
     } 
-    // else if (!passwordPattern.test(values.password)) {
-    //   errors.password = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and be 8 characters or longer";
-    // } 
     else {
       errors.password = "";
     }
